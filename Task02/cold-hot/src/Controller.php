a9fb8699d5666da927c0cd1d847778909499ad1e
AlekseyUvarov <105549441+AlekseyUvarov@users.noreply.github.com>
2022-12-13 01:28:14 +0300

    Revert "Task02"
    This reverts commit 33213926eefbd898ef551bd7c1b9b78cabedb41e.

--- Task02/cold-hot/src/Controller.php
+++
@@ -1,9 +0,0 @@
-<?php
-    namespace AlekseyUvarov\cold_hot\Controller;
-    use function AlekseyUvarov\cold_hot\View\showGame;
-
-    function startGame(){
-        echo "Game started".PHP_EOL;
-        showGame();
-    }
-?>
--- Task02/cold-hot/src/View.php
+++
@@ -1,7 +0,0 @@
-<?php
-    namespace AlekseyUvarov\cold_hot\View;
-
-    function showGame(){
-        echo "Game shown".PHP_EOL;
-    }
-?>
